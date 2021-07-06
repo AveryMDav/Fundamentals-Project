7a5903597d27e810985620fc3cfb9f8c9469c4af
Avery.Davenport <averymdavenport@gmail.com>
2021-07-05 18:58:29 -0600

    commit 2

---
+++ index.js
@@ -0,0 +1,62 @@
+const registerNewUser = document.querySelector('#user');
+const logInUser = document.querySelector('#log-in');
+// connects to buttons in html
+
+const baseURL = `http://localhost:4500/api`
+const catchError = function(err){console.log(err)}
+// useful constant variables
+
+registerNewUser.addEventListener('click',registerSubmitHandler);
+logInUser.addEventListener('click',loginSubmitHandler);
+// creates interactivity for buttons when clicked
+
+const register = function(body) {
+    axios.post(`${baseURL}/register`, body)
+    .then(res => {})
+    .catch(catchError)
+}
+
+function registerSubmitHandler(e) {
+    alert('IT WORKS')
+    // e.preventDefault();
+
+    // let firstname = document.querySelector("#fName");
+    // let lastname = document.querySelector("#lName");
+    // let username = document.querySelector("#uName");
+
+    // let bodyObj = {
+    //     firstname: firstname.value,
+    //     lastname: lastname.value,
+    //     username: username.value
+    // };
+
+    // register(bodyObj)
+
+    // firstname.value = '';
+    // lastname.value = '';
+    // username.value = '';
+}
+// functions for creating a new user
+
+const login = function(body){
+    axios.post(`${baseURL}/login`, body)
+    .then(res => {})
+    .catch(catchError)
+}
+
+
+function loginSubmitHandler(e) {
+    alert('IT WORKS')
+    // e.preventDefault();
+
+    // let username = document.querySelector('#loginUName');
+
+    // let bodyObj = {
+    //     username: username.value
+    // };
+
+    // login(bodyObj);
+
+    // username.value = '';
+}
+// functions for logging in a user
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors")
+const app = express();
+const path = require("path");
+const controller = require("./controller");
+const { } = controller;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')));
+
+// app.get("/api/live-decorah", getDecorah);
+// app.get("/api/live-decorah", getDecorah);
+// app.get("/api/live-decorah", getDecorah);
+
+
+
+let port = process.env.PORT || 4500
+app.listen(port, () => console.log(`Server is assimilating mortals on ${port}`))
